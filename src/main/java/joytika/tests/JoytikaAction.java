3d0ef035b18607720d6d06860dc4b754c211a3c2
Мария <nimasha@mail.ru>
2015-12-04 15:35:30 +0400

    Логины в JoytikaActions

--- src/main/java/joytika/tests/JoytikaAction.java
+++ src/main/java/joytika/tests/JoytikaAction.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by NasyaS on 03.12.2015.
@@ -20,6 +22,42 @@ public class JoytikaAction {
         element.click();
     }
 
-    static void tutorClickCross(){}
+    static void tutorClickCross() {
+        WebElement cross = driver.findElement(By.xpath("/html/body[@id='body']/div[@class='modal-tutor js-login _dota _active']/div[@id='blockLogin']/div[@class='block-tutor-inner']/div[@class='block-tutor-inner-close windowClose']"));
+        cross.click();
+    }
+
+    static void openMenu() {
+        WebElement menu = driver.findElement(By.xpath("/html/body[@id='body']/div[@class='Header-fixed _animation']/div[@class='Header-menu']"));
+        menu.click();
+    }
+
+    static void myAccountClick () {
+        WebElement myaccount = driver.findElement(By.xpath("/html/body[@id='body']/div[@class='Menu _animation']/div[@class='Menu-body nano _animation has-scrollbar']/div[@class='nano-content']/div[@class='Nav']/a[@class='ripple Nav-item '][1]"));
+        myaccount.click();
+    }
+
+    static void login (String vkName, String vkPassword) {
+
+        WebDriverWait wait = new WebDriverWait(driver,300);
+
+        JoytikaAction.openMenu();
+        JoytikaAction.myAccountClick();
+
+        WebElement loginButton = driver.findElement(By.xpath(".//*[@id='blockLogin']/div/div[4]"));
+        loginButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='box']/div/input[6]")));
+
+        WebElement vkLoginField = driver.findElement(By.xpath(".//*[@id='box']/div/input[6]"));
+        vkLoginField.sendKeys(vkName);
+
+        WebElement vkPasswordField = driver.findElement(By.xpath(".//*[@id='box']/div/input[7]"));
+        vkPasswordField.sendKeys(vkPassword);
+
+        WebElement vkLoginButton= driver.findElement(By.xpath(".//*[@id='install_allow']"));
+        vkLoginButton.click();
+
+    }
 
 }
